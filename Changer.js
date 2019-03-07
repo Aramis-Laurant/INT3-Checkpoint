b3efeb217203e03c8ee1aee3911ccbddeb515574
Aramis <Aramis_stLaurant@outlook.com>
2019-03-07 09:56:53 -0800

    Started the project

---
+++ Changer.js
@@ -0,0 +1,8 @@
+//Create a variable for newwrgbcolor
+var newBGColor = document.getElementById("newwrgbcolor");
+var ColorChangeButton = document.getElementById("ColorChangeButton");
+
+ColorChangeButton.addEventListener("click", function () {
+    document.body.style.backgroundColor = newBGColor.value;
+
+})
